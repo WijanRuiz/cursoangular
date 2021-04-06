11aeae6a46c2238fdc36df917b378badb872d7d1
wijan <wiji180892@gmail.com>
2021-04-06 16:22:12 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeroComponent } from './components/primero/primero.component';
+import { SegundoComponent } from './components/segundo/segundo.component';
+import { TerceroComponent } from './tercero/tercero.component';
+import { CuartoComponent } from './components/cuarto/cuarto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeroComponent,
+    SegundoComponent,
+    TerceroComponent,
+    CuartoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/primero/primero.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-primero',
+  templateUrl: './primero.component.html',
+  styleUrls: ['./primero.component.css']
+})
+export class PrimeroComponent implements OnInit {
+  @Input() name = 'Wijan';
+  constructor() {
+    setTimeout(() => this.name = 'Hola', 5000);
+   }
+
+   public rename = (event: KeyboardEvent) => {
+    const input: HTMLInputElement = event.target as HTMLInputElement;
+    this.name = input.value;
+   }
+
+  ngOnInit(): void {
+  }
+
+}
